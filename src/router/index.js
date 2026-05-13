320b83aeacc7f916f072f30f33fdbabb93a5b413
Tom Tang <tom.tang@riversoft.com.tw>
2026-05-13 11:08:39 +0800

    feat: page update

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, saved) {
     if (saved) return saved
